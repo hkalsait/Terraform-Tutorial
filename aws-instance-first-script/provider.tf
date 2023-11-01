f85e78292e8d774a44ae734ee2fe82a05d7b98d5
hkalsait <87933596+hkalsait@users.noreply.github.com>
2023-11-01 18:55:53 +0530

    Update provider.tf

--- aws-instance-first-script/provider.tf
+++ aws-instance-first-script/provider.tf
@@ -1,4 +1,14 @@
+terraform {
+  required_version = ">= 1.0"
+  required_providers {
+    aws = {
+        source = "hashicorp/aws"
+    }
+}
+
 provider "aws" {
   region  = "${var.region}"
   version = "~> 2.0"
+}
+
 }
